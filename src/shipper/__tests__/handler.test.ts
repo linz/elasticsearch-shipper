497afda43b65eabf96077f446953c98862d8a168
Blayne Chard <bchard@linz.govt.nz>
2020-08-26 15:21:31 +1200

    feat: allow keys to be dropped from log lines before insertion
    this adds "dropKeys" to stream configuration that will remove the keys from the log object before inserting the line into elastic search

--- src/shipper/__tests__/handler.test.ts
+++ src/shipper/__tests__/handler.test.ts
@@ -1,7 +1,10 @@
 'use strict';
 import { expect } from 'chai';
-import { eventTime } from '../../shipper/log';
-import { splitJsonString } from '../../shipper/log.handle';
+import sinon from 'sinon';
+import { Log } from '../../logger';
+import { eventTime, LogObject } from '../../shipper/log';
+import { processCloudWatchData, splitJsonString } from '../../shipper/log.handle';
+import { LogShipper } from '../shipper.config';
 
 describe('splitJSONString', () => {
   it('works without a callback', () => {
@@ -9,6 +12,73 @@ describe('splitJSONString', () => {
     expect(result).to.be.an('Array');
     expect(result).to.have.lengthOf(3);
     expect(result).to.deep.equal(['{a}', '{b}', '{c}']);
+  });
+});
+
+describe('processData', () => {
+  let shipper: LogShipper;
+  const sandbox = sinon.createSandbox();
+
+  const logLine = {
+    owner: '123',
+    logGroup: '/aws/lambda/foo',
+    logStream: 'foo-bar',
+    subscriptionFilters: [],
+    messageType: 'something',
+    logEvents: [{ id: '1', timestamp: Date.now(), message: JSON.stringify({ key: 'value', toDrop: 'something' }) }],
+  };
+
+  beforeEach(() => {
+    shipper = new LogShipper({} as any);
+    shipper.config.accounts = [
+      {
+        id: '123',
+        name: 'fake-config',
+        logGroups: [
+          {
+            filter: '/aws/lambda/foo**',
+            prefix: 'foo-index',
+            index: 1,
+          },
+        ],
+      },
+    ];
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should save a log line', async () => {
+    const saveStub = sandbox.stub(shipper.es, 'save');
+    await processCloudWatchData(shipper, logLine, Log);
+    expect(saveStub.callCount).equal(1);
+    expect(shipper.es.body.length).eq(2);
+    const firstIndexLine = shipper.es.body[0] as any;
+    expect(firstIndexLine.index._index).equal('foo-index-123-' + new Date().toISOString().substring(0, 10));
+    const firstLogLine = shipper.es.body[1] as LogObject;
+    expect(firstLogLine['key']).eq('value');
+    expect(firstLogLine['toDrop']).eq('something');
+  });
+
+  it('should drop keys', async () => {
+    const saveStub = sandbox.stub(shipper.es, 'save');
+    shipper.config.accounts[0].logGroups[0].dropKeys = ['toDrop'];
+
+    await processCloudWatchData(shipper, logLine, Log);
+    expect(saveStub.callCount).equal(1);
+    expect(shipper.es.body.length).eq(2);
+    const firstLogLine = shipper.es.body[1] as LogObject;
+    expect(firstLogLine['key']).eq('value');
+    expect(firstLogLine['toDrop']).eq(undefined);
+  });
+
+  it('should drop indexes', async () => {
+    const saveStub = sandbox.stub(shipper.es, 'save');
+    shipper.config.accounts[0].logGroups[0].drop = true;
+    await processCloudWatchData(shipper, logLine, Log);
+    expect(saveStub.callCount).equal(0);
+    expect(shipper.es.body.length).eq(0);
   });
 });
 
