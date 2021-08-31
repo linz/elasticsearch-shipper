b67b9bb2215ef44d1e0d041e2e69ba6e02cce652
Blayne Chard <bchard@linz.govt.nz>
2021-08-31 13:19:44 +1200

    fix: avoid running regexps on large inputs
    Large inputs cause the regexp to block the event loop

---
+++ src/shipper/log.funcs/__tests__/tag.test.ts
@@ -0,0 +1,47 @@
+import { expect } from 'chai';
+import { LogObject } from '../../type';
+import { onLogTag } from '../tag';
+
+describe('onLogTag', () => {
+  let msg = { message: '' } as LogObject;
+  beforeEach(() => {
+    const tags: string[] = [];
+    msg = { message: '', '@tags': tags } as LogObject;
+  });
+
+  for (const line of [
+    'END RequestId: be0262fa-d7f9-47b8-985e-9bb41e77b624',
+    'REPORT RequestId: be0262fa-d7f9-47b8-985e-9bb41e77b624	Duration: 10.23 ms	Billed Duration: 11 ms	Memory Size: 2048 MB	Max Memory Used: 385 MB',
+    'START RequestId: be0262fa-d7f9-47b8-985e-9bb41e77b624 Version: $LATEST',
+  ]) {
+    const reqType = line.split(' ')[0];
+    it('should tag lambda logs :' + reqType, () => {
+      msg.message = line;
+      const ret = onLogTag(msg);
+      expect(ret).to.eq(undefined);
+      expect(msg['@tags']).deep.eq(['Lambda log']);
+    });
+  }
+
+  it('should drop flow logs', () => {
+    msg.message =
+      '2 418528898914 eni-ababababb 255.255.255.222 255.255.255.159 443 36215 6 17 6419 1630295292 1630295304 ACCEPT OK';
+    const ret = onLogTag(msg);
+    expect(ret).to.eq(true);
+    expect(msg['@tags']).deep.eq(['Flow log']);
+  });
+
+  it('should skip large log lines', () => {
+    msg.message = 'END RequestId: be0262fa-d7f9-47b8-985e-9bb41e77b624'.padEnd(2049, '-');
+    const ret = onLogTag(msg);
+    expect(ret).to.eq(undefined);
+    expect(msg['@tags']).deep.eq([]);
+  });
+
+  it('should not skip largeish log lines', () => {
+    msg.message = 'END RequestId: be0262fa-d7f9-47b8-985e-9bb41e77b624'.padEnd(2048, '-');
+    const ret = onLogTag(msg);
+    expect(ret).to.eq(undefined);
+    expect(msg['@tags']).deep.eq(['Lambda log']);
+  });
+});
