645f9ba8ec362d87bfbdab89e91b9c9807583d2a
Blayne Chard <bchard@linz.govt.nz>
2022-09-06 11:07:11 +1200

    feat: export logHandler as the lambda entry

--- src/shipper/index.ts
+++ src/shipper/index.ts
@@ -1,8 +1,9 @@
-import 'source-map-support/register';
-
-export { handler, s3 } from './app';
+export { s3 } from './app';
 export { ElasticSearch } from './elastic';
 export { LogShipper } from './shipper.config';
 export { LogObject } from './type';
 import { lf } from '@linzjs/lambda';
+import { handler } from './app';
+
+export const logHandler = handler;
 export const Log = lf.Logger;
