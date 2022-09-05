1fb0c0684474a2d9d60ca7c8b289e0dba4b4508a
Blayne Chard <bchard@linz.govt.nz>
2022-09-06 08:54:13 +1200

    refactor: apply lint fixes

--- src/shipper/log.funcs/__tests__/tag.test.ts
+++ src/shipper/log.funcs/__tests__/tag.test.ts
@@ -58,7 +58,8 @@ describe('onLogTag', () => {
   });
 
   it('should accept a combined log format message', () => {
-    msg.message = '127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "https://google.com" "curl/1.2.3"';
+    msg.message =
+      '127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "https://google.com" "curl/1.2.3"';
     accessLogTest(msg);
   });
 
