408f969a56e50ec38e94481447935b6b1dedf7ce
Blayne Chard <bchard@linz.govt.nz>
2022-09-06 14:15:25 +1200

    feat: export env const

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { LogObject, LogProcessFunction } from './shipper/type.js';
 export * from './config/config.elastic.js';
 export * from './config/config.js';
 export * from './shipper/index.js';
+export { Env } from './env.js';
